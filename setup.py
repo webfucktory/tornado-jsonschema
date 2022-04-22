a70b83398b2b5fcfad52bf44b7d90ab317ae0429
Alberto Giancarlo Ferrario <me@albertogferrario.com>
2022-04-22 23:39:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def get_version():
@@ -33,7 +33,7 @@ setup(
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/webfucktory/tornado-jsonschema",
-    packages=find_packages(),
+    packages=['tornado_jsonschema'],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
